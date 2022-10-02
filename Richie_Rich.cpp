21fbaef87d20ea93a3c8063e1d23fad637163d86
Aryan <2002ak2002@gmail.com>
2022-10-02 23:29:31 +0530

    ArrayDay2

---
+++ Richie_Rich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, X;
+
+        cin >> A >> B >> X;
+
+        cout << (B - A) / X << endl;
+    }
+
+    return 0;
+}
